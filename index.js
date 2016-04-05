8c90d9ba916a9e7c6b9c2cb27371a9e72a46fbd7
Ilya Kaminsky <ilyakam@gmail.com>
2016-04-04 23:01:03 -0700

    chore(*): initial setup
    Set up a skeleton project for a node module to be submitted to npmjs.com
    There is no functionality in this release; it only contains boilerplate
    code in order to hit the ground running on future projects. Test for
    JavaScript code style (Standard), unit tests, and code coverage.
    
    Add `.editorconfig` to ensure that all code submissions are uniform
    Add `.gitignore` to ensure that unnecessary files do not get committed
    Add `package.json` to prepare this module for npmjs.com
    Add Codeship, Coveralls, and Dependency status badges to `README.md`
    
    Closes #2

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  /**
+   * @name hello
+   * @description Temporary placeholder for this project
+   * @returns {String} Simply 'world' for the time being
+   */
+  hello: function () {
+    return 'world'
+  }
+}
