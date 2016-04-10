f77faab569e73afec1a106b96525d4166c093e20
Ilya Kaminsky <ilyakam@users.noreply.github.com>
2016-04-10 02:13:52 -0700

    feat(gulp-pug-linter): create plugin (#6)
    Create the plugin to lint Pug errors. Make sure that it works by
    reading local configuration files using the existing `pug-lint` CLI.
    Ignore `Stream` files. Check `Buffer` files. When a lint error is
    encountered, decorate the file with an array of `.pugLinter.errors`.
    
    Create a helper method to report on previously-encountered errors.
    Once errors are encountered, log them using the basic `gulp-util`
    logger. If the reporter is set to fail, throw a `PluginError` to break
    the stream. Otherwise, continue the stream uninterrupted.
    
    Ensure that every line of code is covered with unit tests.
    Annotate functions where it makes sense. Write a friendly `README`
    complete with installation instructions and usage examples.

---
+++ test/reporter.spec.js
@@ -0,0 +1,91 @@
+/* eslint-env mocha */
+var expect = require('chai').expect
+var gutil = require('gulp-util')
+var mockery = require('mockery')
+var sinon = require('sinon')
+
+describe('#reporter()', function () {
+  var gulpUtil
+  var reporter
+  var stream
+
+  describe('when the stream contains PugLinter errors', function () {
+    var streamFile = new gutil.File({
+      base: 'base',
+      contents: new Buffer(''),
+      cwd: __dirname,
+      path: 'path.pug'
+    })
+
+    streamFile.pugLinter = {errors: ['some error']}
+
+    it('should print the error', function () {
+      gulpUtil = {log: function () {}}
+
+      sinon.stub(gulpUtil)
+
+      mockery.enable({
+        useCleanCache: true,
+        warnOnUnregistered: false
+      })
+
+      mockery.registerMock('gulp-util', gulpUtil)
+
+      reporter = require('../reporter')
+
+      stream = reporter()
+
+      stream.write(streamFile)
+
+      stream.end()
+
+      expect(gulpUtil.log.calledWith('some error'))
+        .to.be.ok
+
+      gulpUtil.log.restore()
+
+      mockery.disable()
+
+      mockery.deregisterAll()
+    })
+
+    it('should throw an error when set to fail', function () {
+      function shouldThrowError () {
+        reporter = require('../reporter')
+
+        stream = reporter('fail')
+
+        stream.write(streamFile)
+
+        stream.end()
+      }
+
+      expect(shouldThrowError)
+        .to.throw('some error')
+    })
+  })
+
+  describe('when the stream does not contain PugLinter errors', function () {
+    var streamFile = new gutil.File({
+      base: 'base',
+      contents: new Buffer(''),
+      cwd: __dirname,
+      path: 'path.pug'
+    })
+
+    it('should not throw an error', function () {
+      function shouldNotThrowError () {
+        reporter = require('../reporter')
+
+        stream = reporter('fail')
+
+        stream.write(streamFile)
+
+        stream.end()
+      }
+
+      expect(shouldNotThrowError)
+        .to.not.throw()
+    })
+  })
+})
